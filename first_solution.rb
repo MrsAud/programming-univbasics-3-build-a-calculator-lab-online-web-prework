2f0c39fbc2e5944e8b4617c581ba955ca417be36
Audrea Cook <audreacwork@gmail.com>
2019-07-03 19:01:54 +0000

    Automatically backed up by Learn

--- first_solution.rb
+++ first_solution.rb
@@ -1,35 +1,44 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@MrsAud 
+34
+0 63 learn-co-students/programming-univbasics-3-build-a-calculator-lab-online-web-prework
+ Code  Issues 0  Pull requests 62  Projects 0  Security  Insights
+programming-univbasics-3-build-a-calculator-lab-online-web-prework/lib/math.rb
+@maxwellbenton maxwellbenton creates math.rb, sets starter code
+d34d06c on Apr 29
+24 lines (12 sloc)  188 Bytes
+  
+ Jump to definition is still being calculated for this commit. Check back in a bit. Beta
+
+Learn more or give us feedback
 def addition(num1, num2)
-  sum = num1 + num2
-  puts sum
-  sum
+
 end
 
 def subtraction(num1, num2)
-  difference = num1 - num2
-  puts difference
-  difference
+
 end
 
 def division(num1, num2)
-  quotient = num1 / num2
-  puts quotient
-  quotient
+
 end
 
 def multiplication(num1, num2)
-  product = num1 * num2
-  puts product
-  product
+
 end
 
 def modulo(num1, num2)
-  remainder = num1 % num2
-  puts remainder
-  remainder
+
 end
 
 def square_root(num)
-  root = Math.sqrt(num)
-  puts root
-  root
+
 end

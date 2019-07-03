ff4334f15e78f3555cd4a6c55376ff6e9eaa6200
Audrea Cook <audreacwork@gmail.com>
2019-07-03 19:02:08 +0000

    Automatically backed up by Learn

--- first_solution.rb
+++ first_solution.rb
@@ -1,24 +1,3 @@
-Skip to content
- 
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@MrsAud 
-34
-0 63 learn-co-students/programming-univbasics-3-build-a-calculator-lab-online-web-prework
- Code  Issues 0  Pull requests 62  Projects 0  Security  Insights
-programming-univbasics-3-build-a-calculator-lab-online-web-prework/lib/math.rb
-@maxwellbenton maxwellbenton creates math.rb, sets starter code
-d34d06c on Apr 29
-24 lines (12 sloc)  188 Bytes
-  
- Jump to definition is still being calculated for this commit. Check back in a bit. Beta
-
-Learn more or give us feedback
 def addition(num1, num2)
 
 end
